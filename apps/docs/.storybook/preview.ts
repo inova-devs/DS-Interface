18069b2e48be832b4fcd26cfb6a35168a3a1b92d
Kalil Aziz Santos Chami <kalilazizsantoschami@hotmail.com>
2023-04-06 10:11:55 -0300

    storybook

---
+++ apps/docs/.storybook/preview.ts
@@ -0,0 +1,31 @@
+import type { Preview } from "@storybook/react";
+import { withThemeByDataAttribute } from "@storybook/addon-styling";
+
+import "../src/styles/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+/* snipped for brevity */
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "dark",
+    attributeName: "data-mode",
+  }),
+];
+
+export default preview;
